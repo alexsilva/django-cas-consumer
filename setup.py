6aaa1b94600924d9b6cda95c23735f85ac8b9b7c
Alex Sandro <geniofuturo@gmail.com>
2016-08-23 16:23:11 -0300

    change version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-cas-consumer',
-    version='0.1dev',
+    version='0.2dev',
     description='A "consumer" for the Central Authentication Service (http://jasig.org/cas)',
     author='Chris Williams',
     author_email='chris@nitron.org',
